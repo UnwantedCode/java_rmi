4ff60131c08dd89869c185d25b6fa73cd153045d
adamtarkowski <adam@dharsa.com>
2024-11-22 02:41:47 +0100

    sarus row v1

--- src/agent/AgentImpl.java
+++ src/agent/AgentImpl.java
@@ -21,12 +21,12 @@ public class AgentImpl extends UnicastRemoteObject implements Agent {
 
         if (isMainDiagonal) {
             result = (matrix[0][0] * matrix[1][1] * matrix[2][2]) +
-                    (matrix[0][1] * matrix[1][2] * matrix[2][0]) +
-                    (matrix[0][2] * matrix[1][0] * matrix[2][1]);
+                    (matrix[1][0] * matrix[2][1] * matrix[0][2]) +
+                    (matrix[2][0] * matrix[0][1] * matrix[1][2]);
         } else {
             result = (matrix[0][2] * matrix[1][1] * matrix[2][0]) +
-                    (matrix[0][1] * matrix[1][0] * matrix[2][2]) +
-                    (matrix[0][0] * matrix[1][2] * matrix[2][1]);
+                    (matrix[1][2] * matrix[2][1] * matrix[0][0]) +
+                    (matrix[2][2] * matrix[0][1] * matrix[1][0]);
         }
 
         System.out.println("[" + LocalDateTime.now() + "] Agent: Wynik częściowy obliczony: " + result);
